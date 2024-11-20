1bc0ef880d7b146420f489daaa36e62d3afab594
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2024-11-19 22:06:55 -0500

    first commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@web/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      Hola santiago!
+      <Button>Button</Button>
+    </div>
+  );
+}
