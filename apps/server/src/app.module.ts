8de03b7e8ad3d5cfe7bc318044f60c4197bc4a5e
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2024-11-19 22:24:41 -0500

    feat(trpc): integrate TRPC with NestJS and add client-side support

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrpcModule } from '@server/trpc/trpc.module';
 
 @Module({
-  imports: [],
+  imports: [TrpcModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@web/components/ui/button";
+import {trpc} from "@web/app/trpc"
+import ClientSide from "@web/app/clientside"
 
-export default function Home() {
+export default async function Home() {
+  const { greeting } = await trpc.hello.query({ name: `Tom` });
+  
   return (
     <div>
       Hola santiago!
+      <p>I am server side: {greeting}</p>
+      <ClientSide />
       <Button>Button</Button>
     </div>
   );
